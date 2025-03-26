5570d3159a2c56b987bbe0228844460dfd8e670d
Darien Romero <a20156090@pucp.edu.pe>
2025-03-26 11:45:34 -0500

    carousel

---
+++ src/app/dashboard/card/page.tsx
@@ -0,0 +1,52 @@
+'use client';
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+
+export default function Page() {
+  return (
+    <div className="grid grid-cols-1 sm:grid-cols-3 gap-2">
+      {
+        "123456789".split("").map((_, i) => (
+          <Card key={i}>
+            <CardHeader>
+              <CardTitle>Card Title</CardTitle>
+              <CardDescription>Card Description</CardDescription>
+            </CardHeader>
+            <CardContent>
+              <p>Card Content</p>
+            </CardContent>
+            <CardFooter>
+              <Button variant="ghost">info</Button>
+              <Button >más</Button>
+            </CardFooter>
+          </Card>
+        ))
+      }
+          <Card className="w-[350px]">
+      <CardHeader>
+        <CardTitle>Create project</CardTitle>
+        <CardDescription>Deploy your new project in one-click.</CardDescription>
+      </CardHeader>
+      <CardContent>
+        <form>
+          <div className="grid w-full items-center gap-4">
+            <div className="flex flex-col space-y-1.5">
+              <Label htmlFor="name">Name</Label>
+              <Input id="name" placeholder="Name of your project" />
+            </div>
+            <div className="flex flex-col space-y-1.5">
+              <Label htmlFor="framework">Framework</Label>
+            </div>
+          </div>
+        </form>
+      </CardContent>
+      <CardFooter className="flex justify-between">
+        <Button variant="outline">Cancel</Button>
+        <Button>Deploy</Button>
+      </CardFooter>
+    </Card>
+    </div>
+  );
+}
