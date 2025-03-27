c360e1518d4421a6fdcf62e0226c1ac2b6b81366
Darien Romero <a20156090@pucp.edu.pe>
2025-03-27 10:45:09 -0500

    context menu

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -17,6 +17,9 @@ const links = [
   { name: "carousel", href: "carousel" },
   { name: "checkbox", href: "checkbox" },
   { name: "command", href: "command" },
+  { name: "combobox", href: "combobox" },
+  { name: "contextmenu", href: "contextmenu" },
+  { name: "menubar", href: "menubar" },
 ].sort((a, b) => a.name.localeCompare(b.name));
 
 export default function DashboardLayout({
@@ -155,7 +158,7 @@ export default function DashboardLayout({
               </div>
             </div>
           </main>
-          <footer className="bg-white md:flex md:items-center md:justify-between shadow rounded-lg p-4 md:p-6 xl:p-8 my-6 mx-4">
+          <footer className="bg-white md:flex md:items-center md:justify-between shadow rounded-lg p-4 md:p-6 xl:p-8 my-6 mx-4 print:hidden">
             <ul className="flex items-center flex-wrap mb-6 md:mb-0">
               <li>
                 <a
---
+++ src/app/dashboard/menubar/page.tsx
@@ -0,0 +1,31 @@
+'use client'
+import { Menubar, MenubarContent, MenubarItem, MenubarMenu, MenubarSeparator, MenubarShortcut, MenubarTrigger } from "@/components/ui/menubar";
+import { useRouter } from "next/navigation"
+
+export default function Page() {
+
+  const router = useRouter();
+
+  return (
+    <Menubar>
+      <MenubarMenu>
+        <MenubarTrigger>File</MenubarTrigger>
+        <MenubarContent>
+          <MenubarItem onClick={()=> 
+            setTimeout(() => {
+              print()
+            }, 100)
+          }>
+            Print <MenubarShortcut>⌘T</MenubarShortcut>
+          </MenubarItem>
+          <MenubarItem>New Window</MenubarItem>
+          <MenubarSeparator />
+          <MenubarItem>Share</MenubarItem>
+          <MenubarSeparator />
+          <MenubarItem>Print</MenubarItem>
+        </MenubarContent>
+      </MenubarMenu>
+    </Menubar>
+
+  )
+}
